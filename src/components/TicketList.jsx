e7cc18b225a6e9a4d1ae2fe2a2daab2cd140a1ce
Andrew Bates and Jake Ash <andrew@>
2020-02-17 15:57:12 -0800

    Add states to react queue

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,43 +1,24 @@
 import React from 'react';
 import Ticket from './Ticket';
+import PropTypes from 'prop-types';
 
-var masterTicketList = [
-  {
-    names: 'Potato and Kaley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Fox image not displaying on page, can only see duck?'
-  },
-  {
-    names: 'Imani and Jacob',
-    location: '9F',
-    issue: 'Donkey picture not displaying on hover in Zoology app.l :()'
-  }
-
-];
-
-
-function TicketList(){
+function TicketList(props){
+  console.log(props.ticketList);
   return (
     <div>
-      <style jsx>{`
-        div {
-          background-color: blue;
-        }
-      `}</style>
       <hr/>
-      {masterTicketList.map((ticket, index) =>
+      {props.ticketList.map((ticket) =>
         <Ticket names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          key={index}/>
+          key={ticket.id}/>
       )}
     </div>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
